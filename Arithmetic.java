cc6d67b1d50acf67162ce90f972c68969b618176
ShivalingamSurigi <surigishivalingam4@gmail.com>
2026-09-08 11:41:07 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,18 @@
+class Arithmetic
+{
+public static void main(String[] args)
+{
+try
+{
+int a=10/0;
+}
+catch (ArithmeticException e)
+{
+System.out.println("Error:"+e.getMessage());
+}
+finally
+{
+System.out.println("finally exception done");
+}
+}
+}
